cb434b4e34451554ba132fd3a4b0be3f6c7d2eb8
romc-odoo <romc@odoo.com>
2025-04-15 08:51:53 +0200

    [IMP] hr_skills: update resume on job position change
    task-4686721

--- addons/hr_skills/models/hr_employee.py
+++ addons/hr_skills/models/hr_employee.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 from odoo.tools import convert
 
@@ -39,6 +41,8 @@ class HrEmployee(models.Model):
         res = super().write(vals)
         if 'department_id' in vals:
             self.employee_skill_ids._create_logs()
+        if 'job_id' in vals:
+            self._create_job_experience()
         return res
 
     def _load_scenario(self):
@@ -47,3 +51,25 @@ class HrEmployee(models.Model):
         if demo_tag:
             return
         convert.convert_file(self.env, 'hr_skills', 'data/scenarios/hr_skills_scenario.xml', None, mode='init', kind='data')
+
+    def _create_job_experience(self):
+        resume_lines = []
+        exp_line_type = self.env.ref('hr_skills.resume_type_experience', raise_if_not_found=False)
+        for employee in self:
+            job_experience_last_15_days = self.resume_line_ids.filtered(
+                lambda resume_line: resume_line.line_type_id == (exp_line_type or self.env['hr.resume.line.type'])
+                    and resume_line.date_start >= fields.Date().today() - relativedelta(days=15)
+            )
+            if job_experience_last_15_days:  # Overwrite the most recent experience in the last 15 days
+                job_experience_last_15_days = max(job_experience_last_15_days, key=lambda exp: exp.date_start)
+                job_experience_last_15_days.unlink()
+
+            resume_lines.append({
+                'employee_id': employee.id,
+                'name': employee.company_id.name or '',
+                'date_start': fields.Date().today(),
+                'description': employee.job_title or '',
+                'line_type_id': exp_line_type and exp_line_type.id,
+            })
+
+        self.env['hr.resume.line'].create(resume_lines)

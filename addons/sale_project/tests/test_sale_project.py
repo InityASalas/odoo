32991e1a11f143555544ca130d019bd1dc6b4236
iemu-odoo <iemu@odoo.com>
2025-04-15 16:04:35 +0530

    [IMP] sale_project: link milestone when product invoicing policy is milestone
    When a product is configured as follows:
       Product Type: Service
        Create on Order - Nothing
       Invoicing Policy: Based on milestones
    
    user creates a sale order linked to a project,the milestone stat
    button will be visible on the sale order. From there, the user can
    also create new milestones.
    
    task-4648575

--- addons/sale_project/tests/test_sale_project.py
+++ addons/sale_project/tests/test_sale_project.py
@@ -1286,3 +1286,32 @@ class TestSaleProject(TestSaleProjectCommon):
         so.action_confirm()
         self.assertFalse(self.product_order_service2.project_id.task_ids)
         self.assertFalse(sol.task_id)
+
+    def test_so_with_create_on_order_nothing_milestone_products(self):
+        """
+        Check whether a milestone is linked with the sale order.
+
+        Steps:
+          - Create a sale order
+          - Use a delivered milestone product
+          - Confirm the sale order
+          - Check the milestone is created and linked
+        """
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'project_id': self.project_global.id,
+            'order_line': [
+                Command.create({
+                    'product_id': self.product_service_delivered_milestone.id,
+                    'product_uom_qty': 1,
+                }),
+            ],
+        })
+
+        sale_order.action_confirm()
+
+        self.assertEqual(
+            sale_order.milestone_count, 1,
+            "There should be a milestone linked to the sale order line."
+        )

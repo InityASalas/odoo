fdf2a7a5ad209e5dc474e5bef9201a6c4e1d9982
amdi-odoo <amdi@odoo.com>
2025-04-24 14:28:34 +0200

    [IMP] event: improve slot calendar
    Improve the slot calendar by:
    
    - Making sure the quick create form displayed in mobile view
    is the one for the slots.
    
    The desktop view is using the multi create, but the mobile view
    isn't. Replacing the default quick create form view to use
    the slot form.
    
    - Adding a title to the slot calendar ADD mode form to make
    sure users understand what those fields are about.
    
    Task-4743999

---
+++ addons/event/static/src/views/event_slot_calendar/event_slot_calendar_controller.js
@@ -0,0 +1,18 @@
+import { CalendarController } from "@web/views/calendar/calendar_controller";
+import { _t } from "@web/core/l10n/translation";
+
+export class EventSlotCalendarController extends CalendarController {
+
+    /**
+     * Rename mobile quick create dialog. No quick create on desktop (using multi create).
+     * Load model after save to show created record.
+     */
+    getQuickCreateFormViewProps(record) {
+        return {
+            ...super.getQuickCreateFormViewProps(record),
+            onRecordSaved: () => this.model.load(),
+            title: _t("New Slot"),
+        };
+    }
+
+}

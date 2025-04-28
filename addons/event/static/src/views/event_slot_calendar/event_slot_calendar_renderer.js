f3d30fb00dd7664b7a2b492ab55faa2a8f2441af
amdi-odoo <amdi@odoo.com>
2025-04-28 11:49:39 +0200

    [IMP] event: add slot calendar overlay
    Purpose
    =======
    Ease the slots creation in batch which is frustrating as
    it's raising a Validation error each time the user tries to
    create a slot outside of the event time range.
    
    Specification
    =============
    Greying out the days outside of the event time range to make
    them unselectable. This greatly reduces the chances of having
    a Validation error as the users can now visually see the event
    time range on the calendar.
    
    As only the days are greyed out and not the hours, a Validation error
    can still happen if the user tries to create a slot on the start or end
    date of the event outside of its range. For example if the event starts
    at 10AM on that day but the user tries to create a slot starting before that.
    
    Task-4743999

--- addons/event/static/src/views/event_slot_calendar/event_slot_calendar_renderer.js
+++ addons/event/static/src/views/event_slot_calendar/event_slot_calendar_renderer.js
@@ -2,9 +2,30 @@ import { CalendarCommonRenderer } from "@web/views/calendar/calendar_common/cale
 import { CalendarRenderer } from "@web/views/calendar/calendar_renderer";
 import { CalendarYearRenderer } from "@web/views/calendar/calendar_year/calendar_year_renderer";
 
+import { useEffect } from "@odoo/owl";
+
 export class EventSlotCalendarCommonRenderer extends CalendarCommonRenderer {
     // Display end time and hide title on the full calendar library event.
     static eventTemplate = "event.EventSlotCalendarCommonRenderer.event";
+
+    setup() {
+        super.setup(...arguments);
+        useEffect(
+            (fcEls) => {
+                // Add overlay to disable days outside of event time range.
+                const [rangeStartDate,] = this.props.model.meta.context.event_start_date.split(' ');
+                const [rangeEndDate,] = this.props.model.meta.context.event_end_date.split(' ');
+                for (const fcEl of fcEls) {
+                    fcEl.querySelectorAll(".fc-day:not(.fc-col-header-cell)").forEach((dayEl) => {
+                        if (dayEl.dataset.date < rangeStartDate || dayEl.dataset.date > rangeEndDate) {
+                            dayEl.classList.add("bg-secondary", "bg-opacity-25", "pe-none");
+                        }
+                    });
+                }
+            },
+            () => [[this.fc.el]],
+        );
+    }
 }
 
 export class EventSlotCalendarRenderer extends CalendarRenderer {

82ed41305cb7932c82ed513a2dbca9509be120c5
Antoine Boonen <aboo@odoo.com>
2025-04-30 11:53:04 +0200

    [IMP] l10n_ro_edi: synchronize with ANAF
    This commit adds the possibility to synchronize invoice and bill status
    at once.
    
    The endpoint `ListaMesajeFactura` returns a list of messages to process.
    Those messages can:
    - Return an invoice status update with a success or an error;
    - Return a new bill from another vendor already validated by the
    authority.
    
    The messages contain a message ID to recover and download the official
    data from the ANAF server: the invoice/bill XML and the signature.
    
    (documentation)[https://mfinante.gov.ro/static/10/eFactura/prezentare%20api%20efactura.pdf]
    
    When synchronizing, we retrieve the new messages from the last 24 hours
    and update the database content accordingly:
    - in case of invoice status update, update the relevant invoice/bill
    status and download the signature in case of success
    (note that this was already done before, we only call the relevant
    methods to do so);
    - in case of a new vendor bill, we download the bill XML and signature,
    create a new in_invoice move and fill it up with the XML content. In
    case a bill with the same index is already in the database, we do
    nothing to avoid duplicate. If a similar bill (same date and amount) is
    found but not yet validated, we only validate it.
    
    The synchronization can be triggered manually through the option in the
    list view and it is automatically triggered every 4 hours with a CRON.
    
    Lastly, the created bill journal can be set up in the Accounting
    settings so that users can specify their favorite journals for automated
    ANAF bills.
    
    task-4588800

--- addons/l10n_ro_edi/models/utils.py
+++ addons/l10n_ro_edi/models/utils.py
@@ -11,7 +11,10 @@ from odoo.tools.safe_eval import json
 NS_UPLOAD = {"ns": "mfp:anaf:dgti:spv:respUploadFisier:v1"}
 NS_STATUS = {"ns": "mfp:anaf:dgti:efactura:stareMesajFactura:v1"}
 NS_HEADER = {"ns": "mfp:anaf:dgti:efactura:mesajEroriFactuta:v1"}
-NS_DOWNLOAD = {"cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"}
+NS_DOWNLOAD = {
+    "cac": "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
+    "cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
+}
 NS_SIGNATURE = {"ns": "http://www.w3.org/2000/09/xmldsig#"}
 
 
@@ -206,9 +209,71 @@ def _request_ciusro_download_answer(company, key_download, session):
                 extracted_data['invoice'] = {
                     'name': root.findtext('.//cbc:ID', namespaces=NS_DOWNLOAD),
                     'amount_total': root.findtext('.//cbc:TaxInclusiveAmount', namespaces=NS_DOWNLOAD),
-                    'due_date': datetime.strptime(root.findtext('.//cbc:DueDate', namespaces=NS_DOWNLOAD), '%Y-%m-%d').date(),
+                    'buyer_vat': root.findtext('.//cac:AccountingSupplierParty//cbc:CompanyID', namespaces=NS_DOWNLOAD),
+                    'seller_vat': root.findtext('.//cac:AccountingCustomerParty//cbc:CompanyID', namespaces=NS_DOWNLOAD),
+                    'date': datetime.strptime(root.findtext('.//cbc:IssueDate', namespaces=NS_DOWNLOAD), '%Y-%m-%d').date(),
                     'attachment_raw': file_bytes,
                 }
 
     zip_ref.close()
     return extracted_data
+
+
+def _request_ciusro_synchronize_invoices(company, session, nb_days=1):
+    """
+    This method makes a "Fetch Messages" (GET/listaMesajeFactura) request to the Romanian SPV.
+    After processing the response, if messages were indeed fetched, it will fetch the content
+    of said messages.
+
+    Possible returns:
+    - {'error': `str`} if there was a failing response from a bad request;
+    - {'sent_invoices_messages': [`dict`], 'sent_invoices_refused_messages': [`dict`], 'received_bills_messages': [`dict`]}
+    where `dict` is {'data_creare': `str`, 'cif': `str`, 'id_solicitare': `str`, 'detalii': `str`,
+    'tip': 'FACTURA TRIMISA'|'ERORI FACTURA'|'FACTURA PRIMITA', 'id': `str`, answer: <`_request_ciusro_download_answer`>}; representing a message.
+    sent_invoices_messages will contain all message validating an invoice, sent_invoices_refused_messages will contain all messages refusing an invoice
+    and received_bills_messages will contain all message representing received bills.
+
+    :param company: ``res.company`` object
+    :param session: ``requests.Session()`` object
+    :param nb_days(optional,default=1): ``int`` the number of days for which the request should be made, min=1, max=60
+    :return: {'error': `str`} | {'sent_invoices_messages': [`dict`], 'sent_invoices_refused_messages': [`dict`], 'received_bills_messages': [`dict`]}
+    """
+    result = make_efactura_request(
+        session=session,
+        company=company,
+        endpoint='listaMesajeFactura',
+        method='GET',
+        params={'zile': nb_days, 'cif': company.vat.replace('RO', '')},
+    )
+    if 'error' in result:
+        return {'error': result['error']}
+
+    try:
+        msg_content = json.loads(result['content'].decode())
+    except ValueError:
+        return {'error': _("The SPV data could not be parsed.")}
+
+    if eroare := msg_content.get('eroare'):
+        return {'error': eroare}
+
+    received_bills_messages = []
+    sent_invoices_accepted_messages = []
+    sent_invoices_refused_messages = []
+    for message in msg_content.get('mesaje'):
+        message['answer'] = _request_ciusro_download_answer(
+            key_download=message['id'],
+            company=company,
+            session=session,
+        )
+        if message['tip'] == 'FACTURA TRIMISA':
+            sent_invoices_accepted_messages.append(message)
+        if message['tip'] == 'ERORI FACTURA':
+            sent_invoices_refused_messages.append(message)
+        elif message['tip'] == 'FACTURA PRIMITA':
+            received_bills_messages.append(message)
+
+    return {
+        'sent_invoices_accepted_messages': sent_invoices_accepted_messages,
+        'sent_invoices_refused_messages': sent_invoices_refused_messages,
+        'received_bills_messages': received_bills_messages
+    }

98d4a4840c0907c81ad5c6dd176b8717b5497540
Shubham Thanki <shut@odoo.com>
2025-05-13 18:58:35 +0530

    [IMP] various: improve login, signup and password reset UX
    - We have added a `fa-eye` icon that reveals the password to the user when they
      click on it.
    
    - Also added an alert message below the password input field, notifying the user
      if he has CAPS LOCK active.
    - Since we added the CAPS LOCK alert below the password input field, we had to
      move the password meter above the input field.
    - Added password strength titles (Weak, Medium, Strong) just beside the
      password meter.
    - Made sure the password meter becomes visible only when we start typing
      the password.
    
    - In the reset password window, the fields with the label `Your Name` &
     `Your Email` has been removed.
      (This would make sure the user data is not revealed even if
      the reset password link is compromised/ made public)
    - Previously, when the user successfully reset the password, the user
      would login automatically, but that should not happen in case the reset
      password link gets compromised.
    - So now the user needs to login again and also display a text-success message
      stating the password was successfully reset.
    - Switched the order of the NAME and EMAIL fields in the signup form.
    
    TECHNICAL
    ---------
    
    - Added both things (eye icon to show password, and caps lock on alert) at
      4 places/forms --- login form, signup form, password reset form,
      portal connection & security tab.
    - Wrote some custom CSS to ensure the box shadow on the input fields also
      wraps around the `fa-eye` button.
    
    task-3959739

--- addons/web/static/src/public/show_password.js
+++ addons/web/static/src/public/show_password.js
@@ -2,26 +2,23 @@ import { Interaction } from "@web/public/interaction";
 import { registry } from "@web/core/registry";
 
 export class ShowPassword extends Interaction {
-    static selector = "#showPass";
+    static selector = ".o_show_password";
     dynamicContent = {
-        _root: { "t-on-pointerdown": this.showText },
+        _root: {
+            "t-on-click": () =>
+                (this.passwordEl.type = this.passwordEl.type === "password" ? "text" : "password"),
+        },
+        "i": {
+            "t-att-class": () => ({
+                "fa-eye": this.passwordEl.type === "password",
+                "fa-eye-slash": this.passwordEl.type === "text",
+            }),
+        },
     };
 
     setup() {
-        this.passwordEl = this.el.closest(".input-group").querySelector("#password");
-    }
-
-    showText() {
-        this.passwordEl.setAttribute("type", "text");
-        this.addListener(
-            document.body,
-            "pointerup",
-            () => this.passwordEl.setAttribute("type", "password"),
-            { once: true },
-        );
+        this.passwordEl = this.el.closest(".input-group").querySelector("input[type='password']");
     }
 }
 
-registry
-    .category("public.interactions")
-    .add("website.show_password", ShowPassword);
+registry.category("public.interactions").add("web.show_password", ShowPassword);

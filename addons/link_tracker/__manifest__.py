5179846d61fc279fea3e7f2a58b1074fc97e7645
amdi-odoo <amdi@odoo.com>
2025-05-15 15:59:37 +0200

    [IMP] link_tracker: improve views
    Improve link tracker form, list, graph and search views
    as the current ones lack clarity, making it hard for the
    marketing team to track the campaign, source, and other details.
    
    Task-4781806

--- addons/link_tracker/__manifest__.py
+++ addons/link_tracker/__manifest__.py
@@ -14,6 +14,11 @@ Shorten URLs and use them to track clicks and UTMs
         'views/utm_campaign_views.xml',
         'security/ir.model.access.csv',
     ],
+    'assets': {
+        'web.assets_backend_lazy': [
+            'link_tracker/static/src/views/**',
+        ],
+    },
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }

a4ac259f0338e82a3d9d330de2a5bae167fd5559
Sarah Bellefroid <sbel@odoo.com>
2025-05-19 08:38:30 +0200

    [FIX] point_of_sale: update archived products when opening pos
    Currently, when archiving products, they will still appear in the pos
    session.
    
    Steps to reproduce:
    -------------------
    * Open a shop and close it (to store products in cache)
    * Archive any product from the previous shop
    * Open the shop again
    > Observation: The product is still visible in the product screen.
    
    Why the fix:
    ------------
    Compared to previous versions, we are now caching products for performance
    issue. At the opening of a session, cache gets updated with any new value/
    change that occurred after the last modification of the config parameters.
    
    Currently, at session opening, it is only possible to add new value, not
    to remove useless ones.
    
    Initially there were 2 different directions for the fix:
    * Compare the records in the cache from those being loaded, and delete
    the records we did not received anymore.
    > This is not a good solution. With products, for example, when you load
    the session it will only load the active products that have been modified after
    the last modification date on the pos config. Let's say only 1 product was
    modified, then the pos receives only that product (+the special products).
    With that idea of a fix we would have deleted all products exept 1. Not ideal.
    
    * Remove records by asking the db which should be removed.
    > We send a request with the ids of the records in cache to know which
    ones are not needed anymore. This has the adventage of possible adaptability
    depending on the type of record we treat with (for example, not keep expired
    loyalty programs).
    
    To avoid potential performance issue that could happen if we were to make
    a rpc call for all models loaded in the pos we currently only limit it to
    a subset of models.
    
    opw-4657471

--- addons/point_of_sale/static/src/app/services/data_service.js
+++ addons/point_of_sale/static/src/app/services/data_service.js
@@ -241,7 +241,7 @@ export class PosData extends Reactive {
                 );
 
                 for (const [model, values] of Object.entries(data)) {
-                    const local = localData[model] || [];
+                    let local = localData[model] || [];
 
                     if (this.opts.uniqueModels.includes(model) && values.length > 0) {
                         this.indexedDB.delete(
@@ -250,6 +250,13 @@ export class PosData extends Reactive {
                         );
                         localData[model] = values;
                     } else {
+                        if (local.length > 0 && this.opts.cleanupModels.includes(model)) {
+                            const to_remove = await this.orm.call(model, "cleanup_data", [
+                                local.map((r) => r.id),
+                            ]);
+                            local = local.filter((r) => !to_remove.includes(r.id));
+                            this.indexedDB.delete(model, to_remove);
+                        }
                         localData[model] = local.concat(values);
                     }
                 }

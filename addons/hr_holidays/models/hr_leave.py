d9e188a8dd236902da2f13d63a715bf6d44f30fe
David Beguin <dbe@odoo.com>
2025-05-22 14:57:01 +0200

    [FIX] hr_holidays: fix access to get_mandatory_days while validating leave

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -423,7 +423,8 @@ class HrLeave(models.Model):
     def _compute_has_mandatory_day(self):
         date_from, date_to = min(self.mapped('date_from')), max(self.mapped('date_to'))
         if date_from and date_to:
-            mandatory_days = self.employee_id._get_mandatory_days(
+            # Sudo to get access to version fields on employee (job_id)
+            mandatory_days = self.employee_id.sudo()._get_mandatory_days(
                 date_from.date(),
                 date_to.date())
 

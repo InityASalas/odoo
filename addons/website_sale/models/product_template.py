b46818a4242e7ae98c62d17a75c1ebab6c91876c
Victor Feyens <vfe@odoo.com>
2025-05-23 19:11:47 +0200

    [IMP] website_(*_)sale(_*): uom support
    Allow chosing unit of measures on the ecommerce:
    * product page
    * product configurator
    
    Also revamp the display of cart information to:
    
    * display the quantities as bubble on the product image
    * separate the attribute details from the product name
    * display the chosen uom (if multiple available)
    
    task-4485463
    
    Co-authored-by: Mathieu Notté <mano@odoo.com>

--- addons/website_sale/models/product_template.py
+++ addons/website_sale/models/product_template.py
@@ -356,7 +356,7 @@ class ProductTemplate(models.Model):
         return next(self._get_possible_combinations(parent_combination), False) is not False
 
     def _get_combination_info(
-        self, combination=False, product_id=False, add_qty=1.0, only_template=False,
+        self, combination=False, product_id=False, add_qty=1.0, uom_id=False, only_template=False,
     ):
         """ Return info about a given combination.
 
@@ -404,6 +404,7 @@ class ProductTemplate(models.Model):
 
         combination = combination or self.env['product.template.attribute.value']
         website = request.website.with_context(self.env.context)
+        uom = self.env['uom.uom'].sudo().browse(uom_id) or self.uom_id.sudo()
 
         if not product_id and not combination and not only_template:
             combination = self._get_first_possible_combination()
@@ -441,6 +442,7 @@ class ProductTemplate(models.Model):
             **self._get_additionnal_combination_info(
                 product_or_template=product_or_template,
                 quantity=add_qty or 1.0,
+                uom=uom,
                 date=fields.Date.context_today(self),
                 website=website,
             )
@@ -467,7 +469,7 @@ class ProductTemplate(models.Model):
 
         return combination_info
 
-    def _get_additionnal_combination_info(self, product_or_template, quantity, date, website):
+    def _get_additionnal_combination_info(self, product_or_template, quantity, uom, date, website):
         """Computes additional combination info, based on given parameters
 
         :param product_or_template: `product.product` or `product.template` record
@@ -487,6 +489,7 @@ class ProductTemplate(models.Model):
         pricelist_price, pricelist_rule_id = pricelist._get_product_price_rule(
             product=product_or_template,
             quantity=quantity,
+            uom=uom,
             target_currency=currency,
         )
 
@@ -497,7 +500,7 @@ class ProductTemplate(models.Model):
                 product=product_or_template,
                 quantity=quantity or 1.0,
                 date=date,
-                uom=product_or_template.uom_id,
+                uom=uom,
                 currency=currency,
             )
 

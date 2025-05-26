b0e27dbc44c32ed2b78070c251881b43b821b96f
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-05-26 13:33:21 +0000

    [REF] modules: cache get_module_icon
    Add a cache on getting the icon for known modules.

--- odoo/modules/module.py
+++ odoo/modules/module.py
@@ -365,8 +365,12 @@ def get_resource_from_path(path: str) -> tuple[str, str, str] | None:
 
 
 def get_module_icon(module: str) -> str:
-    fpath = f"{module}/static/description/icon.png"
+    """ Get the path to the module's icon. Invalid module names are accepted. """
+    manifest = Manifest.get_module(module, display_warning=False)
+    if manifest and 'icon' in manifest.__dict__:
+        return manifest.icon
     try:
+        fpath = f"{module}/static/description/icon.png"
         tools.file_path(fpath)
         return "/" + fpath
     except FileNotFoundError:

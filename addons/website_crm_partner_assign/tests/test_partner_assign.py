706a895a1ab671bdac059bb6d0daf28aeaa8191f
Mohammed Shekha <msh@odoo.com>
2025-06-03 12:44:43 +0530

    [IMP] website_crm_partner_assign: Enable and adapt some tours
    test_03_reditor_not_salesman test was previously disabled, enabled the tour again.
    
    This commit updates the tour steps to align with the new DOM and
    re-enables the associated test.

--- addons/website_crm_partner_assign/tests/test_partner_assign.py
+++ addons/website_crm_partner_assign/tests/test_partner_assign.py
@@ -289,8 +289,6 @@ class TestPublish(HttpCase):
         self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_can_publish_partner', login="testtest")
         self.assertTrue(self.partner.website_published, "Partner should have been published")
 
-    # TODO master-mysterious-egg fix error
-    @unittest.skip("prepare mysterious-egg for merging")
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_03_reditor_not_salesman(self):
         self.user_test.group_ids = [

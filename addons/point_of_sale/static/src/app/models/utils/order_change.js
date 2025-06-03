e39a4758a46e3a8fcc611e88e6e728bb9762e4cc
parp-odoo <parp@odoo.com>
2025-06-03 17:46:30 +0530

    [FIX] point_of_sale: handle notes in self-order preparation receipts
    since self-order preparation receipt currently uses the same template as the
    POS preparation receipt, which by default prints an empty
    stringified array (`[]`) in the `internal_note` section.
    
    This commit moves the `getStrNotes` method to `order_change.js`,
    To display properly formatted internal notes in self-order receipts.
    
    Task-4827509

--- addons/point_of_sale/static/src/app/models/utils/order_change.js
+++ addons/point_of_sale/static/src/app/models/utils/order_change.js
@@ -1,3 +1,21 @@
+export const getStrNotes = (note) => {
+    console.log(note);
+    if (!note || typeof note !== "string") {
+        return "";
+    }
+
+    let noteStr = note;
+    try {
+        noteStr = JSON.parse(note)
+            .map((n) => n.text)
+            .join(", ");
+    } catch {
+        noteStr = note;
+    }
+    console.log("After :", noteStr);
+    return noteStr;
+};
+
 export const changesToOrder = (order, orderPreparationCategories, cancelled = false) => {
     const toAdd = [];
     const toRemove = [];
@@ -7,7 +25,8 @@ export const changesToOrder = (order, orderPreparationCategories, cancelled = fa
         ? Object.values(orderChanges.orderlines)
         : Object.values(order.last_order_preparation_change.lines);
 
-    for (const lineChange of linesChanges) {
+    for (let lineChange of linesChanges) {
+        lineChange = { ...lineChange, note: getStrNotes(lineChange.note) };
         if (lineChange["quantity"] > 0 && !cancelled) {
             toAdd.push(lineChange);
         } else {
@@ -68,7 +87,7 @@ export const getOrderChanges = (order, orderPreparationCategories) => {
                 product_id: product.id,
                 attribute_value_names: orderline.attribute_value_ids.map((a) => a.name),
                 quantity: quantityDiff,
-                note: note,
+                note: getStrNotes(note),
                 pos_categ_id: product.pos_categ_ids[0]?.id ?? 0,
                 pos_categ_sequence: product.pos_categ_ids[0]?.sequence ?? 0,
                 display_name: product.display_name,
@@ -118,7 +137,7 @@ export const getOrderChanges = (order, orderPreparationCategories) => {
                     basic_name: lineResume["basic_name"],
                     display_name: lineResume["display_name"],
                     isCombo: lineResume["isCombo"],
-                    note: lineResume["note"],
+                    note: getStrNotes(lineResume["note"]),
                     attribute_value_names: lineResume["attribute_value_names"],
                     group: lineResume["group"],
                     quantity: -quantity,

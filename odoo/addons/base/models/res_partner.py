InityASalas/odoo

commit 29f7475437586363e473955315d74aef7d80028c

[IMP] base,web: change "/n" behaviour in display_name
The previous behaviour of /n in the Many2XAutocomplete has
been replaced for the wanted behaviour in odoomark (get breakline).

The res.partner _compute_display_name has been remake to adapt to
this changes. The keys 'address_inline' and 'partner_display_name_hide_company'
have also been removed because they are no longer used.

Task-id: 4749554
Part-of: odoo/odoo#207672
Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

odoo/addons/base/models/res_partner.py
@@ -981,26 +981,37 @@ class ResPartner(models.Model):
 
     @api.depends('complete_name', 'email', 'vat', 'state_id', 'country_id', 'commercial_company_name')
     @api.depends_context(
-        'show_address', 'partner_display_name_hide_company', 'partner_show_db_id', 'address_inline',
+        'show_address', 'partner_show_db_id',
         'show_email', 'show_vat', 'lang', 'formatted_display_name'
     )
     def _compute_display_name(self):
         for partner in self:
-            name = partner.with_context(lang=self.env.lang)._get_complete_name()
-            if partner._context.get('show_address'):
-                name = name + "\n" + partner._display_address(without_company=True)
-            name = re.sub(r'\s+\n', '\n', name)
-            if partner._context.get('partner_show_db_id'):
-                name = f"{name} ({partner.id})"
-            if partner._context.get('address_inline'):
-                splitted_names = name.split("\n")
-                name = ", ".join([n for n in splitted_names if n.strip()])
-            if partner._context.get('show_email') and partner.email:
-                name = f"{name} <{partner.email}>"
-            if partner._context.get('show_vat') and partner.vat:
-                name = f"{name} ‒ {partner.vat}"
-            if partner._context.get("formatted_display_name") and (partner.parent_id or partner.company_name):
-                name = f"{partner.company_name or partner.parent_id.name}<tab>--{partner.name}--"
+            name = partner.name
+            if partner._context.get("formatted_display_name"):
+                if partner.parent_id or partner.company_name:
+                    name = f"{partner.company_name or partner.parent_id.name} \t --{partner.name}--"
+
+                if partner._context.get('show_email') and partner.email:
+                    name = f"{name} \t --{partner.email}--"
+                elif partner._context.get('show_vat') and partner.vat:
+                    name = f"{name} \t --{partner.vat}--"
+                elif partner._context.get('partner_show_db_id'):
+                    name = f"{name} \t --{partner.id}--"
+
+            else:
+                name = partner.with_context(lang=self.env.lang)._get_complete_name()
+                if partner._context.get('partner_show_db_id'):
+                    name = f"{name} ({partner.id})"
+                if partner._context.get('show_email') and partner.email:
+                    name = f"{name} <{partner.email}>"
+                if partner._context.get('show_address'):
+                    name = name + "\n" + partner._display_address(without_company=True)
+
+                if partner._context.get('show_vat') and partner.vat:
+                    if partner._context.get('show_address'):
+                        name = f"{name} \n {partner.vat}"
+                    else:
+                        name = f"{name} - {partner.vat}"
 
             partner.display_name = name.strip()
 

addons/web/static/src/core/utils/strings.js
@@ -133,8 +133,8 @@ export function odoomark(text) {
     const boldEx = /\*\*(.+?)\*\*/g;
     const textMutedEx = /--(.+?)--/g;
     const tagEx = /&#x60;(.+?)&#x60;/g;
-    const brEx = /&lt;br&gt;/g;
-    const tabEx = /&lt;tab&gt;/g;
+    const brEx = /\n/g;
+    const tabEx = /\t/g;
 
     return markup(
         escape(text)

addons/web/static/src/views/fields/relational_utils.js
@@ -362,7 +362,7 @@ export class Many2XAutocomplete extends Component {
         const label = record.__formatted_display_name || record.display_name;
         return {
             value: record.id,
-            label: label ? odoomark(label.split("\n")[0]) : _t("Unnamed"),
+            label: label ? odoomark(label) : _t("Unnamed"),
         };
     }
 

addons/web/static/tests/core/utils/strings.test.js
@@ -147,6 +147,6 @@ test("odoomark", () => {
     expect(odoomark("--test-- something else --test--").toString()).toBe("<span class='text-muted'>test</span> something else <span class='text-muted'>test</span>");
     expect(odoomark("`test`").toString()).toBe(`<span class="o_tag position-relative d-inline-flex align-items-center mw-100 o_badge badge rounded-pill lh-1 text-white bg-primary">test</span>`);
     expect(odoomark("`test` something else `test`").toString()).toBe(`<span class="o_tag position-relative d-inline-flex align-items-center mw-100 o_badge badge rounded-pill lh-1 text-white bg-primary">test</span> something else <span class="o_tag position-relative d-inline-flex align-items-center mw-100 o_badge badge rounded-pill lh-1 text-white bg-primary">test</span>`);
-    expect(odoomark("test<tab>test2").toString()).toBe("test&nbsp;&nbsp;&nbsp;&nbsp;test2");
-    expect(odoomark("test<br>test2").toString()).toBe("test<br/>test2");
+    expect(odoomark("test\ttest2").toString()).toBe("test&nbsp;&nbsp;&nbsp;&nbsp;test2");
+    expect(odoomark("test\ntest2").toString()).toBe("test<br/>test2");
 });

addons/web/static/tests/views/fields/many2one_field.test.js
@@ -1196,75 +1196,6 @@ test("many2one searches with correct value", async () => {
     expect.verifySteps(["search: ", "search: ", "search: p", "search: p"]);
 });
 
-test("many2one search with server returning multiple lines", async () => {
-    const namegets = {
-        2: "fizz\nbuzz\nfizzbuzz",
-        4: "aaa\nAAA\nRecord",
-    };
-
-    onRpc("web_read", async ({ parent }) => {
-        expect.step("web_read");
-        const result = await parent();
-        result[0].trululu = {
-            id: result[0].trululu.id,
-            display_name: namegets[result[0].trululu.id],
-        };
-        return result;
-    });
-    onRpc("web_name_search", () => {
-        expect.step("web_name_search");
-        return Object.keys(namegets).map((id) => ({
-            id: parseInt(id),
-            display_name: namegets[id],
-            __formatted_display_name: namegets[id],
-        }));
-    });
-    onRpc("web_save", ({ args }) => {
-        expect.step("web_save");
-        expect(args[1]).toEqual({
-            trululu: 2,
-        });
-    });
-
-    await mountView({
-        type: "form",
-        resModel: "partner",
-        resId: 1,
-        arch: `
-            <form>
-                <sheet>
-                    <group>
-                        <field name="trululu" />
-                    </group>
-                </sheet>
-            </form>`,
-    });
-    expect.verifySteps(["web_read"]);
-
-    // Initial value
-    expect(".o_field_widget input").toHaveValue("aaa");
-    expect(".o_field_many2one_extra").toHaveInnerHTML("<div>AAA</div><div>Record</div>", {
-        type: "html",
-    });
-
-    // Change the value
-    await contains(".o_field_widget input").edit("fizz", { confirm: false });
-    await runAllTimers();
-    // should display only the first line of the returned value from the server
-    expect.verifySteps(["web_name_search"]);
-    expect(queryAllTexts(".dropdown-menu li:not(.o_m2o_dropdown_option")).toEqual(["fizz", "aaa"]);
-    await contains(".dropdown-menu li").click();
-
-    expect.verifySteps([]);
-    expect(".o_field_widget input").toHaveValue("fizz");
-    expect(".o_field_many2one_extra").toHaveInnerHTML("<div>buzz</div><div>fizzbuzz</div>", {
-        type: "html",
-    });
-
-    await clickSave();
-    expect.verifySteps(["web_save"]);
-});
-
 test("many2one search with trailing and leading spaces", async () => {
     onRpc("web_name_search", ({ kwargs }) => {
         expect.step("search: " + kwargs.name);
@@ -3916,7 +3847,7 @@ test("many2one search with formatted name", async () => {
         {
             id: 1,
             display_name: "Paul Eric",
-            __formatted_display_name: "Test: **Paul** --Eric-- `good guy`<br><tab>More text",
+            __formatted_display_name: "Test: **Paul** --Eric-- `good guy`\n\tMore text",
         },
     ]);
     await mountView({

odoo/addons/base/tests/test_res_partner.py
@@ -1014,11 +1014,6 @@ class TestPartnerAddressCompany(TransactionCase):
         res_bhide = test_partner_bhide.with_context(show_address=1).display_name
         self.assertEqual(res_bhide, "Atmaram Bhide", "name should contain only name if address is not available, without extra commas")
 
-        res_jetha = test_partner_jetha.with_context(show_address=1, address_inline=1).display_name
-        self.assertEqual(res_jetha, "Jethala, Powder gali, Gokuldham Society", "name should contain comma separated name and address")
-        res_bhide = test_partner_bhide.with_context(show_address=1, address_inline=1).display_name
-        self.assertEqual(res_bhide, "Atmaram Bhide", "name should contain only name if address is not available, without extra commas")
-
     def test_accessibility_of_company_partner_from_branch(self):
         """ Check accessibility of company partner from branch. """
         company = self.env['res.company'].create({'name': 'company'})

commit 0a1150a8c40ec8d1cf352b66fafa5bdc97c287db

[FIX] base: remove superfluous whitespace before line breaks in display_name
Strip unnecessary whitespace (spaces, tabs, new line) before line breaks in computed `display_name`,
preventing formatting issues such as unintended blank lines or misaligned address blocks when fields like `street`
or `street2` are empty.

This fixes a regression introduced in commit 29f7475437586363e473955315d74aef7d80028c, where the clean up line
was removed during a refactor.

odoo/addons/base/models/res_partner.py
@@ -1013,6 +1013,7 @@ class ResPartner(models.Model):
                     else:
                         name = f"{name} - {partner.vat}"
 
+            name = re.sub(r'\s+\n', '\n', name)
             partner.display_name = name.strip()
 
     @api.model